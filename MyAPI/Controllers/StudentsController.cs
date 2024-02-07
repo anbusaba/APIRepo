3e04ede68594121589ce87d62eeee45c9f17cc8b
anbusaba <131363097+anbusaba@users.noreply.github.com>
2024-02-07 12:46:58 +0530

    First

---
+++ MyAPI/Controllers/StudentsController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyAPI.Models;
+
+namespace MyAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private List<Student> students = new List<Student>()
+        {
+            new Student()
+            {
+                Id = 1,
+                Name="NEo",
+                Age=25,
+                City="Chennai"
+            },
+            new Student()
+            {
+                Id = 2,
+                Name="Trinity",
+                Age=25,
+                City="Mumbai"
+
+            }
+        };
+
+        [HttpGet]
+        [Route("getStudents")]
+        public async Task< ActionResult<Student>> GetStudents()
+        {
+            return Ok(students); 
+        }
+        [HttpGet]
+        [Route("getStudent")]
+        public async Task<ActionResult<Student>> GetStudent(int id)
+        {
+
+            var student = students.Find(x => x.Id == id);
+            if (student == null)
+                return BadRequest("No Student Found");
+            return Ok(student);
+        }
+
+        [HttpPost]
+        [Route("addStudent")]
+        public async Task<ActionResult<Student>> AddStudent(Student request)
+        {
+
+            students.Add(request);
+            return Ok(students);
+        }
+        [HttpPut]
+        [Route("updateStudent")]
+        public async Task<ActionResult<Student>> UpdateStudent(Student request)
+        {
+
+            var student = students.Find(x => x.Id == request.Id);
+            if (student == null)
+                return BadRequest("No Student Found");
+            student.Name = request.Name;
+            student.Age = request.Age;
+            student.City = request.City;
+            return Ok(students);
+
+
+        }
+        [HttpDelete]
+        [Route("deleteStudent")]
+        public async Task<ActionResult<Student>> DeleteStudent(int id)
+        {
+
+            var student = students.Find(x => x.Id == id);
+            if (student == null)
+                return BadRequest("No Student Found");
+            students.Remove(student);
+            return Ok(students);
+
+
+        }
+    }
+}
+
---
+++ MyAPI/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace MyAPI.Models
+{
+    public class Student
+    {
+        public int Id { get; set; } 
+
+        public string Name { get; set; }=String.Empty;
+
+        public int Age { get; set; }
+
+        public string City {get; set; } = String.Empty;
+    }
+}
